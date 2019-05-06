dd2c20f50d156e1ef1de548327e13d6a419dfcf6
Sean <sean.m.mcqueen@gmail.com>
2019-05-06 13:53:47 -0700

    Updates

---
+++ precache-manifest.7f2219dad7f9ce33c79ca3140f9c4860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dad2b6a448e36f6e3ba",
+    "url": "/pomodoro/static/js/runtime~main.0dad2b6a.js"
+  },
+  {
+    "revision": "60bd542a40a4476d3537",
+    "url": "/pomodoro/static/js/main.60bd542a.chunk.js"
+  },
+  {
+    "revision": "8d5e8abe13d577d390d1",
+    "url": "/pomodoro/static/js/2.8d5e8abe.chunk.js"
+  },
+  {
+    "revision": "60bd542a40a4476d3537",
+    "url": "/pomodoro/static/css/main.3f892419.chunk.css"
+  },
+  {
+    "revision": "a31263fa94cc43a400595760e49b88ba",
+    "url": "/pomodoro/index.html"
+  }
+];
